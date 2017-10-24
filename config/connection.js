688dd70127dc529ff9fa56a86c24b777862f5509
annalisa leote <amleote@gmail.com>
2017-10-24 19:05:39 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWBD_URL);
+ } else {
+  connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com  ",
+  user: " txzbuyul02x95g9n",
+  password: "v0q8n67lwuyts2td",
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
